2507f55cadf50a5d77f183361ab0ee94f44c110d
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2010-09-17 19:46:10 +0200

    [build] Add cb(pre)ignore capabilities to build. Removes dependencies from being installed in case of is_emulator_arch. E.g. omit normal install of package which is being replaced or unused anyway.
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

--- Build.pm
+++ Build.pm
@@ -209,7 +209,7 @@ sub read_config {
       }
       next;
     }
-    if ($l0 eq 'preinstall:' || $l0 eq 'vminstall:' || $l0 eq 'cbpreinstall:' || $l0 eq 'cbinstall:' || $l0 eq 'required:' || $l0 eq 'support:' || $l0 eq 'keep:' || $l0 eq 'prefer:' || $l0 eq 'ignore:' || $l0 eq 'conflict:' || $l0 eq 'runscripts:') {
+    if ($l0 eq 'preinstall:' || $l0 eq 'vminstall:' || $l0 eq 'cbpreinstall:' || $l0 eq 'cbinstall:' || $l0 eq 'cbpreignore:' || $l0 eq 'cbignore:' || $l0 eq 'required:' || $l0 eq 'support:' || $l0 eq 'keep:' || $l0 eq 'prefer:' || $l0 eq 'ignore:' || $l0 eq 'conflict:' || $l0 eq 'runscripts:') {
       my $t = substr($l0, 0, -1);
       for my $l (@l) {
 	if ($l eq '!*') {
@@ -276,7 +276,7 @@ sub read_config {
       warn("unknown keyword in config: $l0\n");
     }
   }
-  for my $l (qw{preinstall vminstall cbpreinstall cbinstall required support keep runscripts repotype patterntype}) {
+  for my $l (qw{preinstall vminstall cbpreinstall cbinstall cbpreignore cbignore required support keep runscripts repotype patterntype}) {
     $config->{$l} = [ unify(@{$config->{$l}}) ];
   }
   for my $l (keys %{$config->{'substitute'}}) {
@@ -415,6 +415,16 @@ sub get_cbpreinstalls {
 sub get_cbinstalls {
   my ($config) = @_;
   return @{$config->{'cbinstall'}};
+}
+
+sub get_cbpreignores {
+  my ($config) = @_;
+  return @{$config->{'cbpreignore'}};
+}
+
+sub get_cbignores {
+  my ($config) = @_;
+  return @{$config->{'cbignore'}};
 }
 
 sub get_runscripts {
