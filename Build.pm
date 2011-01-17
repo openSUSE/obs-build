713266b8e8e406135ab6f615a1d3cd6e1dfe5d25
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2011-01-17 06:48:52 -0700

    Add armv7hl (hardfloat ABI) support. Beside armv7hl, there's also armv7nhl als compatible subarch with additional (neon) flags.
    Tested-by:     Martin Mohring <martin.mohring@5edatasoft.com>
    Reviewed-by:   Martin Mohring <martin.mohring@5edatasoft.com>

--- Build.pm
+++ Build.pm
@@ -29,8 +29,8 @@ sub import {
 my $std_macros = q{
 %define nil
 %define ix86 i386 i486 i586 i686 athlon
-%define arm armv4l armv4b armv5l armv5b armv5el armv5eb armv5tel armv5teb armv6el armv6eb armv7el armv7eb
-%define arml armv4l armv5l armv5tel armv5el armv6el armv7el
+%define arm armv4l armv4b armv5l armv5b armv5el armv5eb armv5tel armv5teb armv6el armv6eb armv7el armv7eb armv7hl armv7nhl armv8el
+%define arml armv4l armv5l armv5tel armv5el armv6el armv7el armv7hl armv7nhl armv8el
 %define armb armv4b armv5b armv5teb armv5eb armv6eb armv7eb
 %define sparc sparc sparcv8 sparcv9 sparcv9v sparc64 sparc64v
 };
