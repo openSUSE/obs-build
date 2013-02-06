dd83bd599006f991277b135816712f0cf4f213c1
Adrian Schröter <adrian@suse.de>
2013-02-06 12:11:46 +0100

    - update aarch64 emulator example

--- emulator/emulator.sh
+++ emulator/emulator.sh
@@ -8,16 +8,18 @@ exit 1
 ### Example for the aarch64 emulator:
 ###
 
-#LOG=/tmp/foundation.$$
-
-#./Foundation_v8 --image img-foundation.axf \
-#                 --block-device "$1" \
-#                 --network=none &> $LOG &
-#sleep 3
-
+LOG=$(mktemp)
+./Foundation_v8 --image ./img-foundation.axf \
+                 --block-device "$1" \
+                 --network=none &> $LOG &
+while test $(grep -c terminal_0: $LOG ) -lt 1; do
+    echo ".."
+    sleep 1
+done
+cat $LOG
 # terminal_0: Listening for serial connection on port 5012
-#PORT=$(head -n 1 $LOG | cut -d " " -f 8)
-#rm -f $LOG
-#telnet localhost $PORT || exit 0
+PORT=$(grep terminal_0: $LOG | head -n 1 | cut -d " " -f 8)
+rm -f $LOG
+telnet 127.0.0.1 $PORT
 
 
