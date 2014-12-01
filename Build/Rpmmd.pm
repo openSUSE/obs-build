52b6b0489925a5d237292fc38d95b938190f81b6
Michael Schroeder <mls@suse.de>
2014-12-01 13:30:35 +0100

    more Rpmmd refactoring

--- Build/Rpmmd.pm
+++ Build/Rpmmd.pm
@@ -24,61 +24,78 @@ use strict;
 
 use XML::Parser;
 
-my @cursor;
-my $data;
-my $result;
-
-sub generic_handle_start {
-  my ($p, $el) = @_;
-  my $h = $cursor[-1]->[1];
-  if (exists $h->{$el}) {
-    $h = $h->{$el};
-    push @cursor, [$el, $h];
-    $cursor[-1]->[2] = '' if $h->{'_text'};
-    $h->{'_start'}->($h, @_) if exists $h->{'_start'};
+sub generic_parse {
+  my ($how, $in, $res, %options) = @_;
+  $res ||= [];
+  my @cursor = ([undef, $how, undef, $res, undef, \%options]);
+  my $p = new XML::Parser(Handlers => {
+    Start => sub {
+      my ($p, $el) = @_;
+      my $h = $cursor[-1]->[1];
+      return unless exists $h->{$el};
+      $h = $h->{$el};
+      push @cursor, [$el, $h];
+      $cursor[-1]->[2] = '' if $h->{'_text'};
+      $h->{'_start'}->($h, \@cursor, @_) if exists $h->{'_start'};
+    },
+    End => sub {
+      my ($p, $el) = @_;
+      if ($cursor[-1]->[0] eq $el) {
+	my $h = $cursor[-1]->[1];
+	$h->{'_end'}->($h, \@cursor, @_) if exists $h->{'_end'};
+	pop @cursor;
+      }
+    },
+    Char => sub {
+      my ($p, $text) = @_;
+      $cursor[-1]->[2] .= $text if defined $cursor[-1]->[2];
+    },
+  }, ErrorContext => 2);
+  if (ref($in)) {
+    $p->parse($in);
+  } else {
+    $p->parsefile($in);
   }
-}
-
-sub generic_handle_char {
-  my ($p, $text) = @_;
-  $cursor[-1]->[2] .= $text if defined $cursor[-1]->[2];
-}
-
-sub generic_handle_end {
-  my ($p, $el) = @_;
-  if (!defined $cursor[-1]->[0] || $cursor[-1]->[0] eq $el) {
-    my $h = $cursor[-1]->[1];
-    $h->{'_end'}->($h, @_) if exists $h->{'_end'};
-    pop @cursor;
-  }
+  return $res;
 }
 
 sub generic_store_text {
-  my ($h, $p, $el) = @_;
-  $data->{$h->{'_tag'}} = $cursor[-1]->[2] if defined $cursor[-1]->[2];
+  my ($h, $c, $p, $el) = @_;
+  my $data = $c->[0]->[4];
+  $data->{$h->{'_tag'}} = $c->[-1]->[2] if defined $c->[-1]->[2];
 }
 
 sub generic_store_attr {
-  my ($h, $p, $el, %attr) = @_;
+  my ($h, $c, $p, $el, %attr) = @_;
+  my $data = $c->[0]->[4];
   $data->{$h->{'_tag'}} = $attr{$h->{'_attr'}} if defined $attr{$h->{'_attr'}};
 }
 
-sub generic_add_result {
-  my ($h, $p, $el, %attr) = @_;
-  return unless $data;
-  if (ref($result) eq 'CODE') {
-    $result->($data);
-  } else {
-    push @$result, $data;
-  }
-  undef $data;
+sub generic_new_data {
+  my ($h, $c, $p, $el, %attr) = @_;
+  $c->[0]->[4] = {};
+  generic_store_attr(@_) if $h->{'_attr'};
 }
 
+sub generic_add_result {
+  my ($h, $c, $p, $el) = @_;
+  my $data = $c->[0]->[4];
+  return unless $data;
+  my $res = $c->[0]->[3];
+  if (ref($res) eq 'CODE') {
+    $res->($data);
+  } else {
+    push @$res, $data;
+  }
+  undef $c->[0]->[4];
+}
 
 my $repomdparser = {
   repomd => {
     data => {
-      _start => \&repomd_handle_data_start,
+      _start => \&generic_new_data,
+      _attr => 'type',
+      _tag => 'type',
       _end => \&generic_add_result,
       location => { _start => \&generic_store_attr, _attr => 'href', _tag => 'location'},
       size => { _text => 1, _end => \&generic_store_text, _tag => 'size'},
@@ -86,79 +103,68 @@ my $repomdparser = {
   },
 };
 
-
-sub repomd_handle_data_start {
-  my ($h, $p, $el, %attr) = @_;
-  $data = {'type' => $attr{'type'}};
-}
-
-sub parse_repomd {
-  my ($fh, $res) = @_;
-  my $p = new XML::Parser(
-  Handlers => {
-    Start => \&generic_handle_start,
-    End => \&generic_handle_end,
-    Char => \&generic_handle_char
-  });
-  @cursor = ([undef, $repomdparser]);
-  $result = $res;
-  $p->parse($fh);
-}
-
-
 my $primaryparser = {
   metadata => {
     'package' => {
-      _start => \&primary_handle_package_start,
+      _start => \&generic_new_data,
+      _attr => 'type',
+      _tag => 'type',
       _end => \&generic_add_result,
       name => { _text => 1, _end => \&generic_store_text, _tag => 'name' },
       arch => { _text => 1, _end => \&generic_store_text, _tag => 'arch' },
       version => { _start => \&primary_handle_version },
       'time' => { _start => \&primary_handle_time },
       format => {
-        'rpm:provides' => { 'rpm:entry' => { _start => \&primary_handle_dep , _tag => 'provides' }, },
-        'rpm:requires' => { 'rpm:entry' => { _start => \&primary_handle_dep , _tag => 'requires' }, },
-        'rpm:conflicts' => { 'rpm:entry' => { _start => \&primary_handle_dep , _tag => 'conflicts' }, },
-        'rpm:obsoletes' => { 'rpm:entry' => { _start => \&primary_handle_dep , _tag => 'obsoletes' }, },
+        'rpm:provides' =>    { 'rpm:entry' => { _start => \&primary_handle_dep , _tag => 'provides' }, },
+        'rpm:requires' =>    { 'rpm:entry' => { _start => \&primary_handle_dep , _tag => 'requires' }, },
+        'rpm:conflicts' =>   { 'rpm:entry' => { _start => \&primary_handle_dep , _tag => 'conflicts' }, },
+        'rpm:recommends' =>  { 'rpm:entry' => { _start => \&primary_handle_dep , _tag => 'recommends' }, },
+        'rpm:suggests' =>    { 'rpm:entry' => { _start => \&primary_handle_dep , _tag => 'suggests' }, },
+        'rpm:supplements' => { 'rpm:entry' => { _start => \&primary_handle_dep , _tag => 'supplements' }, },
+        'rpm:enhances' =>    { 'rpm:entry' => { _start => \&primary_handle_dep , _tag => 'enhances' }, },
+        'rpm:obsoletes' =>   { 'rpm:entry' => { _start => \&primary_handle_dep , _tag => 'obsoletes' }, },
         'rpm:buildhost' => { _text => 1, _end => \&generic_store_text, _tag => 'buildhost' },
-        'rpm:sourcerpm' => { _text => 1, _end => \&generic_store_text, _tag => 'sourcerpm' },
+        'rpm:sourcerpm' => { _text => 1, _end => \&primary_handle_sourcerpm , _tag => 'source' },
 ### currently commented out, as we ignore file provides in createrpmdeps
-#       file => { _text => 1, _end => \&primary_handle_file_end, _tag => 'provides' }
-#       },
+#       file => { _text => 1, _end => \&primary_handle_file_end, _tag => 'provides' },
       },
       location => { _start => \&generic_store_attr, _attr => 'href', _tag => 'location'},
     },
   },
 };
 
-
-sub primary_handle_package_start {
-  my ($h, $p, $el, %attr) = @_;
-  $data = { type => $attr{'type'} };
+sub primary_handle_sourcerpm {
+  my ($h, $c, $p, $el, %attr) = @_;
+  my $data = $c->[0]->[4];
+  return unless defined $c->[-1]->[2];
+  $c->[-1]->[2] =~ s/-[^-]*-[^-]*\.[^\.]*\.rpm$//;
+  $data->{$h->{'_tag'}} = $c->[-1]->[2];
 }
 
 sub primary_handle_version {
-  my ($h, $p, $el, %attr) = @_;
+  my ($h, $c, $p, $el, %attr) = @_;
+  my $data = $c->[0]->[4];
   $data->{'epoch'} = $attr{'epoch'} if $attr{'epoch'};
   $data->{'version'} = $attr{'ver'};
   $data->{'release'} = $attr{'rel'};
 }
 
 sub primary_handle_time {
-  my ($h, $p, $el, %attr) = @_;
+  my ($h, $c, $p, $el, %attr) = @_;
+  my $data = $c->[0]->[4];
   $data->{'filetime'} = $attr{'file'} if $attr{'file'};
   $data->{'buildtime'} = $attr{'build'} if $attr{'build'};
 }
 
 sub primary_handle_file_end {
-  my ($h, $p, $el) = @_;
-  primary_handle_dep($h, $p, $el, 'name', $h->[2]);
+  my ($h, $c, $p, $el) = @_;
+  primary_handle_dep($h, $c, $p, $el, 'name', $c->[-1]->[2]);
 }
 
 my %flagmap = ( EQ => '=', LE => '<=', GE => '>=', GT => '>', LT => '<', NE => '!=' );
 
 sub primary_handle_dep {
-  my ($h, $p, $el, %attr) = @_;
+  my ($h, $c, $p, $el, %attr) = @_;
   my $dep = $attr{'name'};
   return if $dep =~ /^rpmlib\(/;
   if(exists $attr{'flags'}) {
@@ -168,19 +174,15 @@ sub primary_handle_dep {
     $evr .= "-$attr{'rel'}" if defined $attr{'rel'};
     $dep .= " $flagmap{$attr{'flags'}} $evr";
   }
+  my $data = $c->[0]->[4];
   push @{$data->{$h->{'_tag'}}}, $dep;
 }
 
+sub parse_repomd {
+  return generic_parse($repomdparser, @_);
+}
+
 sub parse_primary {
-  my ($fh, $res) = @_;
-  my $p = new XML::Parser(
-  Handlers => {
-    Start => \&generic_handle_start,
-    End => \&generic_handle_end,
-    Char => \&generic_handle_char
-  });
-  @cursor = ([undef, $primaryparser]);
-  $result = $res;
-  $p->parse($fh);
+  return generic_parse($primaryparser, @_);
 }
 
