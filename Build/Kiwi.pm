3cb79891dd6dd805ff39aa03ad0f84aba633e9b1
Adrian Schröter <adrian@suse.de>
2015-04-15 14:37:20 +0200

    support build time source services

--- Build/Kiwi.pm
+++ Build/Kiwi.pm
@@ -21,86 +21,9 @@
 package Build::Kiwi;
 
 use strict;
+use Build::simplexml;
 
 our $bootcallback;
-
-# worst xml parser ever, just good enough to parse those kiwi files...
-# can't use standard XML parsers, unfortunatelly, as the build script
-# must not rely on external libraries
-#
-sub parsexml {
-  my ($xml) = @_;
-
-  my @nodestack;
-  my $node = {};
-  my $c = '';
-  $xml =~ s/^\s*\<\?.*?\?\>//s;
-  while ($xml =~ /^(.*?)\</s) {
-    if ($1 ne '') {
-      $c .= $1;
-      $xml = substr($xml, length($1));
-    }
-    if (substr($xml, 0, 4) eq '<!--') {
-      $xml =~ s/.*?-->//s;
-      next;
-    }
-    die("bad xml\n") unless $xml =~ /(.*?\>)/s;
-    my $tag = $1;
-    $xml = substr($xml, length($tag));
-    my $mode = 0;
-    if ($tag =~ s/^\<\///s) {
-      chop $tag;
-      $mode = 1;	# end
-    } elsif ($tag =~ s/\/\>$//s) {
-      $mode = 2;	# start & end
-      $tag = substr($tag, 1);
-    } else {
-      $tag = substr($tag, 1);
-      chop $tag;
-    }
-    my @tag = split(/(=(?:\"[^\"]*\"|\'[^\']*\'|[^\"\s]*))?\s+/, "$tag ");
-    $tag = shift @tag;
-    shift @tag;
-    push @tag, undef if @tag & 1;
-    my %atts = @tag;
-    for (values %atts) {
-      next unless defined $_;
-      s/^=\"([^\"]*)\"$/=$1/s or s/^=\'([^\']*)\'$/=$1/s;
-      s/^=//s;
-      s/&lt;/</g;
-      s/&gt;/>/g;
-      s/&amp;/&/g;
-      s/&apos;/\'/g;
-      s/&quot;/\"/g;
-    }
-    if ($mode == 0 || $mode == 2) {
-      my $n = {};
-      push @{$node->{$tag}}, $n;
-      for (sort keys %atts) {
-	$n->{$_} = $atts{$_};
-      }
-      if ($mode == 0) {
-	push @nodestack, [ $tag, $node, $c ];
-	$c = '';
-	$node = $n;
-      }
-    } else {
-      die("element '$tag' closes without open\n") unless @nodestack;
-      die("element '$tag' closes, but I expected '$nodestack[-1]->[0]'\n") unless $nodestack[-1]->[0] eq $tag;
-      $c =~ s/^\s*//s;
-      $c =~ s/\s*$//s;
-      $node->{'_content'} = $c if $c ne '';
-      $node = $nodestack[-1]->[1];
-      $c = $nodestack[-1]->[2];
-      pop @nodestack;
-    }
-  }
-  $c .= $xml;
-  $c =~ s/^\s*//s;
-  $c =~ s/\s*$//s;
-  $node->{'_content'} = $c if $c ne '';
-  return $node;
-}
 
 sub unify {
   my %h = map {$_ => 1} @_;
@@ -147,7 +70,7 @@ sub kiwiparse {
   my @requiredarch;
   my $schemaversion = 0;
   my $schemaversion56 = versionstring("5.6");
-  my $kiwi = parsexml($xml);
+  my $kiwi = Build::simplexml::parse($xml);
   die("not a kiwi config\n") unless $kiwi && $kiwi->{'image'};
   $kiwi = $kiwi->{'image'}->[0];
   $schemaversion = versionstring($kiwi->{'schemaversion'}) if $kiwi->{'schemaversion'}; 
