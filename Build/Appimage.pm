ed651e37fc31288bb0ae1cc05dc4e5e2396aeeca
Adrian Schröter <adrian@suse.de>
2017-03-14 17:15:19 +0100

    some appimage support fixes

--- Build/Appimage.pm
+++ Build/Appimage.pm
@@ -1,6 +1,6 @@
 ################################################################
 #
-# Copyright (c) 1995-2014 SUSE Linux Products GmbH
+# Copyright (c) 2017 SUSE Linux Products GmbH
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 or 3 as
