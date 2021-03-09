652eee94c858d31863943ee16bf0b916f41c8afa
Michael Schroeder <mls@suse.de>
2021-03-09 13:35:37 +0100

    Support module config in the Modules parser
    We now also add a "name-stream@context" to the module list of
    a package. To make use of it, the module context and dependencies
    of all modules is stored in a new '/moduleinfo' pseudo element.

--- Build/Modules.pm
+++ Build/Modules.pm
@@ -27,41 +27,72 @@ use YAML::XS;
 
 $YAML::XS::LoadBlessed = 0;
 
+#
+# return a hash that maps NEVRA to modules
+#
 sub parse {
   my ($in, $res, %options) = @_;
 
   $res ||= {};
+  # YAML::XS only alows a GLOB, so we need to do this old fashioned
+  local *FD;
   my $fd;
   if (ref($in)) {
-    $fd = $in;
+    *FD = $in;
   } else {
     if ($in =~ /\.gz$/) {
-      open($fd, '-|', "gzip", "-dc", $in) || die("$in: $!\n");
+      open(FD, '-|', "gzip", "-dc", $in) || die("$in: $!\n");
     } else {
-      open($fd, '<', $in) || die("$in: $!\n");
+      open(FD, '<', $in) || die("$in: $!\n");
     }   
   }
-  my @mod = YAML::XS::LoadFile($fd);
+  my %mods;
+  my @mod = YAML::XS::LoadFile(\*FD);
   for my $mod (@mod) {
     next unless $mod->{'document'} eq 'modulemd';
     my $data = $mod->{'data'};
     next unless $data && ref($data) eq 'HASH';
     my $name = $data->{'name'};
     my $stream = $data->{'stream'};
+    my $context = $data->{'context'};
     my $module = "$name-$stream";
+    my @reqs;
+    my $dependencies = $data->{'dependencies'};
+    $dependencies = $dependencies->[0] if ref($dependencies) eq 'ARRAY';
+    if ($dependencies && ref($dependencies) eq 'HASH') {
+      my $requires = $dependencies->{'requires'};
+      if ($requires && ref($requires) eq 'HASH') {
+	for my $r (sort keys %$requires) {
+	  my $rs = $requires->{$r};
+	  $rs = $rs->[0] unless ref($rs) eq 'ARRAY';
+	  if (@$rs) {
+	    push @reqs, "$r-$rs->[0]";	# XXX: what about the rest?
+	  } else {
+	    push @reqs, $r;		# unversioned
+	  }
+	}
+      }
+    }
+    my $moduleinfo = { 'name' => $module, 'stream' => $data->{'stream'} };
+    $moduleinfo->{'context'} = $context if $context;
+    $moduleinfo->{'requires'} = \@reqs if @reqs;
+    $mods{"$module\@$context"} = $moduleinfo;
     next unless $data->{'artifacts'};
     my $rpms = $data->{'artifacts'}->{'rpms'};
     next unless $rpms && ref($rpms) eq 'ARRAY';
     for my $rpm (@$rpms) {
       my $nrpm = $rpm;
-      $nrpm =~ s/-0:([^-]*-[^-]*\.[^\.]*)$/-$1/;
+      $nrpm =~ s/-0:([^-]*-[^-]*\.[^\.]*)$/-$1/;	# normalize the epoch
       push @{$res->{$nrpm}}, $module;
+      push @{$res->{$nrpm}}, "$module\@$context" if $context;
     }
   }
   # unify
   for (values %$res) {
     $_ = [ sort keys %{ { map {$_ => 1} @$_ } } ] if @$_ > 1;
   }
+  # add moduleinfo
+  $res->{'/moduleinfo'} = [ map {$mods{$_}} sort keys %mods ];
   return $res;
 }
 
