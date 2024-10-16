openSUSE/obs-build

commit 4fecb72aa947ebef438446cec7f5798a852642b3

Simplify build exit status handling a bit

PBuild/Job.pm
@@ -442,17 +442,19 @@ sub finishjob {
     die("fatal build error, see $buildroot/.build.log for more information\n");
   }
   if ($ret == 3) {
-    $ret = 1;
+    $ret = 1;	# map badhost to build failure
   }
-  if ($ret == 9) {
-    rename_build_result($vm, $buildroot);
-    die("XXX: dynamic buildreqs not implemented yet");
-  }
-  
+
+  rename_build_result($vm, $buildroot) if $ret == 0 || $ret == 9;
+
   if (!$ret && (-l "$buildroot/.build.log" || ! -s _)) {
     unlink("$buildroot/.build.log");
     PBuild::Util::writestr("$buildroot/.build.log", undef, "build created no logfile!\n");
     $ret = 1;
+  }
+
+  if ($ret == 9) {
+    die("XXX: dynamic buildreqs not implemented yet");
   }
   if ($ret) {
     my $result = { '_log' => "$buildroot/.build.log" };

commit 3e5bdc914d36c27d72f990a6a3bb6b4fe3b7b3e2

Remove duplicate rename_build_result call
Broken in commit 4fecb72aa947ebef438446cec7f5798a852642b3

PBuild/Job.pm
@@ -460,7 +460,6 @@ sub finishjob {
     my $result = { '_log' => "$buildroot/.build.log" };
     return ('failed', $result);
   }
-  rename_build_result($vm, $buildroot);
   my $result = collect_result($p, $buildroot);
   $result->{'_log'} = "$buildroot/.build.log";
   return ('succeeded', $result);